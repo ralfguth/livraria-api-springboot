86dd66416c4a0136b7026d565b9a578146073d16
Ralf Guth <rsguth@gmail.com>
2021-10-13 18:34:56 -0300

    configura banco de dados

--- src/main/java/dev/ralfguth/livraria/dto/LivroInputDto.java
+++ src/main/java/dev/ralfguth/livraria/dto/LivroInputDto.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,10 +20,16 @@ public class LivroInputDto {
 	@NotBlank
 	@Size(min = 10)
 	private String titulo;
-	@NotBlank
-	private AutorInputDto autor;
+	
 	@Min(100)
 	private int numeroPaginas;
+	
 	@PastOrPresent
 	private LocalDate dataLancamento;
+	
+	@NotNull
+	@JsonAlias("autor_id")
+	private Long autorId;
+	
+	
 }
