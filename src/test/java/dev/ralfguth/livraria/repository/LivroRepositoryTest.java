2b2f111fec043950a83e74c32724cb6cfcde845b
Ralf Guth <rsguth@gmail.com>
2022-02-02 22:13:49 -0300

    test: cria teste que assegura que o banco de testes desta vazio

--- src/test/java/dev/ralfguth/livraria/repository/LivroRepositoryTest.java
+++ src/test/java/dev/ralfguth/livraria/repository/LivroRepositoryTest.java
@@ -1,12 +1,12 @@
 package dev.ralfguth.livraria.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +33,14 @@ class LivroRepositoryTest {
 	@Autowired
 	private TestEntityManager manager;
 
-	@BeforeEach
-	private void setup() {
+	@Test
+	void nãoDeveRetornarNadaComBancoVazio() throws Exception {
+		List<LivrosPorAutorDto> relatorio = repository.livrosDaLivrariaPorAutor();
+		assertTrue(relatorio.isEmpty());
+	}
+
+	@Test
+	void deveriaRetornarNumeroDeAutoresPorLivro() throws Exception {
 		Autor machado = new Autor("Machado de Assis", "machado@email.com", LocalDate.of(1839, 06, 21), "Considerado um dos maiores nomes da literatura do Brasil");
 		manager.persist(machado);
 		Autor clarisse = new Autor("Clarisse Lispector", "clarisse@email.com", LocalDate.of(1920, 12, 10), "Autora de romances, contos, e ensaios, é considerada uma das escritoras brasileiras mais importantes do século XX e a maior escritora judia desde Franz Kafka");
@@ -47,19 +53,12 @@ class LivroRepositoryTest {
 		manager.persist(quincasBorba);
 		Livro horaEstrela = new Livro("A Hora da Estrela", 224, LocalDate.of(2017, 05, 02), clarisse);
 		manager.persist(horaEstrela);
-	}
-
-	@Test
-	void deveriaRetornarNumeroDeAutoresPorLivro() throws Exception {
 		List<LivrosPorAutorDto> relatorio = repository.livrosDaLivrariaPorAutor();
 		assertEquals(2, relatorio.size());
 		Assertions.assertThat(relatorio)
-			.hasSize(2)
-			.extracting(LivrosPorAutorDto::getAutor, LivrosPorAutorDto::getTotalDeLivros, LivrosPorAutorDto::getPercentual)
-			.contains(
-					Assertions.tuple("Machado de Assis", 3l, 0.75), 
-					Assertions.tuple("Clarisse Lispector", 1l, 0.25)
-					);
+		.hasSize(2)
+		.extracting(LivrosPorAutorDto::getAutor, LivrosPorAutorDto::getTotalDeLivros, LivrosPorAutorDto::getPercentual)
+		.contains(Assertions.tuple("Machado de Assis", 3l, 0.75), Assertions.tuple("Clarisse Lispector", 1l, 0.25));
 	}
 
 }
