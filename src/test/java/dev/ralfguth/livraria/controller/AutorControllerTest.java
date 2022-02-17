10e5e6e8d7d0ed0c662eaf03d05cdb8d29fe3069
Ralf Guth <rsguth@gmail.com>
2022-02-16 21:54:48 -0300

    test: cria teste para AutorController

--- src/test/java/dev/ralfguth/livraria/controller/AutorControllerTest.java
+++ src/test/java/dev/ralfguth/livraria/controller/AutorControllerTest.java
@@ -1,15 +1,65 @@
 package dev.ralfguth.livraria.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest // diz que deverá carregar toda a aplicação no teste
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
 class AutorControllerTest {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void naoDeveCadastrarUmAutorComSemDados() throws Exception {
+		String json = "{}";
+		mvc.perform(post("/autor").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isBadRequest());
 	}
+
+	@Test
+	void NaoDeveCadastrarUmAutorSemUmDosDados() throws Exception {
+		String json = "{ " 
+				+ "\"nome\": \"Ralf Guth\", " 
+				+ "\"email\": \"rsguth@gmail.com\", " 
+				+ "\"nascimento\": \"1989-09-23\" " 
+				+ "}";
+		mvc.perform(post("/autor").contentType(MediaType.APPLICATION_JSON).content(json))
+		.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void deveCadastrarUmAutorComOsDadosCompletos() throws Exception {
+		String json = "{ " 
+				+ "\"nome\": \"Ralf Guth\", " 
+				+ "\"email\": \"rsguth@gmail.com\", " 
+				+ "\"nascimento\": \"1989-09-23\", " 
+				+ "\"biografia\": \"Ralf é um guri bom\" " 
+				+ "}";
+
+		String jsonRetorno = "{ " 
+				+ "\"nome\": \"Ralf Guth\" " 
+				+ "}";
+		
+		mvc.perform(post("/autor").contentType(MediaType.APPLICATION_JSON).content(json))
+			.andExpect(status().isCreated())
+			.andExpect(header().exists("Location"))
+			.andExpect(content().json(jsonRetorno));
+	}
+
 
 }
