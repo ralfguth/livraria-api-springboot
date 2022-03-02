6e02c1473cf5ac7cee40dd619e4321246a75793a
Ralf Guth <rsguth@gmail.com>
2022-03-02 19:22:22 -0300

    refator: remove testes nao implementados

--- src/test/java/dev/ralfguth/livraria/controller/AutorControllerTest.java
+++ src/test/java/dev/ralfguth/livraria/controller/AutorControllerTest.java
@@ -1,5 +1,10 @@
 package dev.ralfguth.livraria.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +15,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest 
@@ -61,22 +62,4 @@ class AutorControllerTest {
 			.andExpect(content().json(jsonRetorno));
 	}
 	
-	@Test
-	void deveAtualizarUmAutor() throws Exception {
-		fail("Não Implementado");
-	}
-	
-	@Test
-	void deveRemoverUmAutorPeloId() throws Exception {
-		fail("Não Implementado");
-	}
-	
-	@Test
-	void deveReceberDadosDetalhadosDeAutorQuandoRealizarRequeisicaoPorId() throws Exception {
-		fail("Não Implementado");
-	}
-	
-	
-
-
 }
